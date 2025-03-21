b22958e6deefdf231f59826a68e3421c1ee2c3b1
Augustus1417 <jedxyril.cruz@gmail.com>
2025-03-21 22:15:31 +0000

    undo now covers score standing, reset warns once, removed ;

--- app/index.tsx
+++ app/index.tsx
@@ -1,65 +1,108 @@
-import { Text, View, Pressable, Alert } from "react-native";
+import { Text, View, Pressable, Alert } from "react-native"
 import { useState } from 'react'
-import { StatusBar } from "expo-status-bar";
+import { StatusBar } from "expo-status-bar"
 import styles from './styles'
 
 export default function Index() {
-  const [scores, setScores] = useState({score_1: 0, score_2: 0});
-  const [scoreHistory, setScoreHistory] = useState([""]);
-  const [standing, setStanding] = useState({score_1: 0, score_2: 0});
+  const [scores, setScores] = useState({player_1: 0, player_2: 0})
+  const [scoreHistory, setScoreHistory] = useState([""])
+  const [standing, setStanding] = useState({player_1: 0, player_2: 0})
+  const [scoreRecord, setScoreRecord] = useState([{}])
+  const [alerted, setAlerted] = useState(false)
 
+  const GameWin = (player: keyof typeof scores) => {
+      IncrementStanding(player)
+      setScoreHistory(scores => [...scores, `${player} won`])
+      if (player == "player_1"){
+        Alert.alert("Player 1 won!")
+      } else {
+        Alert.alert("Player 2 won!")
+      }
+      setScoreRecord(currentRecord => [...currentRecord, scores ])
+      NewSet()
+  }
   const IncrementScore = (player: keyof typeof scores) => { 
     const newScores = {...scores, [player]: scores[player] + 1}
-    setScoreHistory(scores => [...scores, player]);
-    setScores(newScores);
-
-    if (newScores.score_1 == 10 && newScores.score_2 == 10){
-    } else if (newScores.score_1 == 11){
-      setStanding(currentStanding => ({...currentStanding, score_1: currentStanding["score_1"] + 1}));
-      Alert.alert('Player 1 Wins!')
-      NewSet()
-    } else if (newScores.score_2 == 11){
-      setStanding(currentStanding => ({...currentStanding, score_2: currentStanding["score_2"] + 1}));
-      Alert.alert('Player 2 Wins!')
-      NewSet()
+    setScores(newScores)
+    if (newScores[player] == 11){
+      GameWin(player)
+      return
     }
+    setScoreHistory(scores => [...scores, player])
+  }
+
+  const IncrementStanding = (player: keyof typeof scores) => {
+    setStanding(currentStanding => ({...currentStanding, [player]: currentStanding[player] + 1}))
   }
 
   const DecrementScore = (player: keyof typeof scores) => {
-    setScores(currentScores => ({...currentScores, [player]: currentScores[player] - 1}));
+    setScores(currentScores => ({...currentScores, [player]: currentScores[player] - 1}))
+  }
+
+  const DecrementStanding = (player: keyof typeof scores) => {
+    setStanding(currentStanding => ({...currentStanding, [player]: currentStanding[player] - 1}))
   }
 
   const NewSet = () => {
-    setScores({score_1: 0, score_2: 0});
-    setScoreHistory([""]);
+    setScores({player_1: 0, player_2: 0})
   }
 
   const Reset = () => {
-    setScores({score_1: 0, score_2: 0});
-    setStanding({score_1: 0, score_2: 0});
-    setScoreHistory([""]);
+    if (!alerted){
+      Alert.alert("WARNING!", "Reset cannot be undone", [{ 
+        text: "Ok",
+        onPress: () => {
+          setAlerted(true)
+          return}
+      }])
+    } else {
+      setScores({player_1: 0, player_2: 0})
+      setStanding({player_1: 0, player_2: 0})
+      setScoreHistory([""])
+      setScoreRecord([{}])
+    }
+  }
+
+  const UndoStanding = () => {
+      const pastRecord = scoreRecord[scoreRecord.length - 1]
+      const newScores = {
+        player_1: pastRecord["player_1" as keyof typeof pastRecord], 
+        player_2: pastRecord["player_2" as keyof typeof pastRecord]}
+      setScoreRecord([...scoreRecord.slice(0, scoreRecord.length-1)])
+      setScores(newScores)
   }
 
   const Undo = () => {
-    const index = scoreHistory.length - 1;
-    if (scoreHistory[index] == "score_1") {
-      DecrementScore("score_1");
-    } else if (scoreHistory[index] == "score_2") {
-      DecrementScore("score_2");
+    const index = scoreHistory.length - 1
+    switch (scoreHistory[index]) {
+      case "player_1":
+        DecrementScore("player_1")
+        break
+      case "player_2":
+        DecrementScore("player_2")
+        break
+      case "player_1 won":
+        DecrementStanding("player_1") 
+        UndoStanding()
+        break
+      case "player_2 won":
+        DecrementStanding("player_2") 
+        UndoStanding()
+        break
     }
-    setScoreHistory([...scoreHistory.slice(0, index)]);
+    setScoreHistory([...scoreHistory.slice(0, index)])
   }
 
   return (
     <View style={styles.main_view}>
       <StatusBar hidden={true}></StatusBar>
 
-      <Pressable onPress={() => IncrementScore("score_1")}>
-        <Text style={styles.score_1}>{String(scores.score_1).padStart(2, "0")}</Text>
+      <Pressable onPress={() => IncrementScore("player_1")}>
+        <Text style={styles.player_1}>{String(scores.player_1).padStart(2, "0")}</Text>
       </Pressable>
 
       <View style= {styles.controls_view}>
-        <Text style={styles.standing}>{standing.score_1}-{standing.score_2}</Text>
+        <Text style={styles.standing}>{standing.player_1}-{standing.player_2}</Text>
         <Pressable onPress={Reset}>
           <Text style={styles.reset}>Reset</Text>
         </Pressable>
@@ -69,10 +112,9 @@ export default function Index() {
         </Pressable>
       </View>
 
-      <Pressable onPress={() => IncrementScore("score_2")}>
-        <Text style={styles.score_2}>{String(scores.score_2).padStart(2, "0")}</Text>
+      <Pressable onPress={() => IncrementScore("player_2")}>
+        <Text style={styles.player_2}>{String(scores.player_2).padStart(2, "0")}</Text>
       </Pressable>
-
     </View>
-  );
+  )
 }
